3f64cc17d17625b189ec97e3526048ea8caa8445
kristish000 <kristish000@gmail.com>
2023-04-19 23:25:00 +0200

    added complete fuction

--- src/App.js
+++ src/App.js
@@ -4,14 +4,24 @@ import { v4 as uuidv4 } from "uuid"
 const LOCAL_STORAGE_KEY = "todoApp.todos"
 
 function App() {
-  const [todos, setTodos] = useState([
-    { id: 1, name: "todo 1", complete: false },
-  ])
+  const [todos, setTodos] = useState([])
   const todoNameRef = useRef()
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if (storedTodos) setTodos((prevTodos) => [...prevTodos, ...storedTodos])
+  }, [])
 
   useEffect(() => {
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
   }, [todos])
+
+  function toggleTodo(id) {
+    const newTodos = [...todos]
+    const todo = newTodos.find((todo) => todo.id === id)
+    todo.complete = !todo.complete
+    setTodos(newTodos)
+  }
 
   function handleAddTodo(e) {
     const name = todoNameRef.current.value
@@ -24,11 +34,11 @@ function App() {
   }
   return (
     <>
-      <TodoList todos={todos} />
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
       <input ref={todoNameRef} type="text" />
       <button onClick={handleAddTodo}>Add Todos</button>
       <button>Clear Completed Todos</button>
-      <div>0 left to do</div>
+      <div>{todos.filter((todo) => !todo.complete).length} left to do</div>
     </>
   )
 }
